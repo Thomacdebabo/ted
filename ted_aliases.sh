b0830fc100f1e90fb10c35de5977889b60aa2990
thomas <git@thomasengineer.com>
2026-01-26 23:44:13 +0100

    start replacing slow python with fast bash

--- ted_aliases.sh
+++ ted_aliases.sh
@@ -2,7 +2,8 @@ alias tedit="obsidian://open?vault=.ted &"
 
 fted() {
     local selected
-    selected=$(find $HOME/.ted/todos/ -name "*.md" | sed "s|^$HOME/.ted/||" | fzf)
+    selected=$(find $HOME/.ted/todos/ -name "*.md" | sed "s|^$HOME/.ted/||" | fzf --preview="bat --color=always $HOME/.ted/{}")
+
     
     if [[ -n "$selected" ]]; then
         echo "$HOME/.ted/$selected"
@@ -28,5 +29,15 @@ donted() {
 sted() {
 	ted show-file $(fted)
 }
+
+ted-id() {
+  basename "$(fted)" | grep -oE '^[A-Z]+[0-9]+'
+}
+
+fzit() {
+  zit start $(ted-id)
+}
+
+
 #alias tedit="code ~/.ted &"
 
